25250048ee6b5950b534aa8c4413ac1e5863488f
Jamiex <romari123456789@gmail.com>
2020-12-24 00:13:36 -0600

    TempSpam

---
+++ TemporarySpam.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+
+def Tempspam():
+    time(10)
+    Counter = 1
+for Counter in range(2):
+    pyautogui.write("Sentence 1")
+    pyautogui.press('enter')
+    pyautogui.write("Sentence 2")
+    pyautogui.press('enter')
+    pyautogui.write("Sentence 3")
+    pyautogui.press('enter')
+    pyautogui.write("Sentence 4")
+    pyautogui.press('enter')
+    pyautogui.write("Sentence 5")
+    pyautogui.press('enter')
+
