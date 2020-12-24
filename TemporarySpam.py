ab02c788257ec82055d970ab95895cea38de91e1
Jamiex <romari123456789@gmail.com>
2020-12-24 00:35:04 -0600

    Comments
    Added comments to the script

--- TemporarySpam.py
+++ TemporarySpam.py
@@ -1,9 +1,12 @@
 import pyautogui
 import time
-
+# pyautogui is importing commands (pyautogui.write) and (pyautogui.press) its library.
 def Tempspam():
+    #def Tempspam(): is the function that the spamming script is in.
     time(10)
+    #time.sleep value amount ->(10) is the duration before the script starts running this amount can be changed.
     Counter = 1
+     #for Counter in range(2): Below is saying that the value of the (Counter)=> 1 then range(2) will loop or send it the amount specified which is (2).
 for Counter in range(2):
     pyautogui.write("Sentence 1")
     pyautogui.press('enter')
@@ -15,4 +18,4 @@ for Counter in range(2):
     pyautogui.press('enter')
     pyautogui.write("Sentence 5")
     pyautogui.press('enter')
-
+    # The text thats between the ("") after each pyautogui.write  command can be changed to anything aslong as its between ("").
